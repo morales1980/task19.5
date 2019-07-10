e8fabb3d5224cc4ba1c26d35a81c8582d9f2fdba
Darek.Szotowicz <dariusz.szotowicz@protonmail.com>
2019-07-10 21:30:21 +0200

    task finished

---
+++ components/Search.js
@@ -0,0 +1,43 @@
+Search = React.createClass({
+  getInitialState() {
+    return {
+      searchingText: ''
+    };
+  },
+
+  handleChange: function(event) {
+    var searchingText = event.target.value;
+    this.setState({
+      searchingText: searchingText
+    });
+
+    if (searchingText.length > 2) {
+      this.props.onSearch(searchingText);
+    }
+  },
+
+  handleKeyUp: function(event) {
+    if (event.keyCode === 13) {
+      this.props.onSearch(this.state.searchingText);
+    }
+  },  
+
+  render: function() {
+    var style = {
+      fontsize: '1.5em',
+      width: '90%',
+      maxWidth: '350px'
+    };
+
+    return (
+      <input
+        type="text"
+        onChange={this.handleChange}
+        onKeyUp={this.handleKeyUp}
+        placeholder="Type search phrase here"
+        style={style}
+        value={this.state.searchingText}
+      />
+    );
+  },
+});
